858eaa735e65a4bc939ff5f5525e4173d9563f6c
Adrian Anton <adanto64@gmail.com>
2016-04-05 16:26:51 +0200

    modified the scanners to use the usual ports

--- scanner.py
+++ scanner.py
@@ -25,12 +25,20 @@ global ips_info
 
 
 def main_manager(ips, ports):
+	global ips_info
+
 	# hosts that responded to the pings
 	pinged_hosts = sP_scan(ips)
 
 	for host in pinged_hosts:
-		print host
-		print sV_scanner(host, ports)
+		if host not in ips_info.keys():
+			ips_info[host] = [{}, sV_scanner(host, ports)]
+		else:
+			ips_info[host][0] = ips_info[host][0]
+			ips_info[host][1] = sV_scanner(host, ports)
+
+	print ips_info
+
 
 
 
@@ -62,18 +70,10 @@ def sV_scanner(host, ports):
 		print 'There was an error with the parameters'
 		usage()
 
-	return nm[host]['tcp'].keys()
-	
-	
-def sV_scanner_Callback(host, info):
-	global ips_info
-
-	print host, info
-
-	ips_info[host] = info
+	return nm[host]
 
 
-# loads the global variables to be able to use them un the program
+# loads the global variables to be able to use them in the program
 def load_globals():
 	global ips_info
 	ips_info = {}
@@ -82,7 +82,7 @@ if __name__ == "__main__":
 
 	load_globals()
 
-	ips = '127.0.0.1'
-	ports = '1-1000'
+	ips = '192.168.100.11/24'
+	ports = '1, 5, 7, 18, 20, 21, 22, 23, 25, 29, 37, 42, 43, 49, 53, 69, 70, 79, 80, 103, 108, 109, 110, 115, 118, 119, 137, 139, 143, 150, 156, 161, 179, 190, 194, 197, 389, 396, 443, 444, 445, 458, 546, 547, 563, 569, 1080'
 
 	main_manager(ips, ports)
