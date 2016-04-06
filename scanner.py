b3241ff53c503355dd6a1f59fe483055909fa058
Adrian Anton <adanto64@gmail.com>
2016-04-06 19:25:26 +0200

    edited the dictionary to store too the host status and timestamp of the record.

--- scanner.py
+++ scanner.py
@@ -3,7 +3,9 @@
 import sys
 import nmap
 
-# import time
+import urllib2
+
+import time
 
 ################################################################################
 ###############################     HELP     ###################################
@@ -23,11 +25,20 @@ def usage():
 ################################################################################
 
 
-global ips_info
+global IPS_INFO
+global INITIAL_TIME
+
+
+class bot:
+	def __init__(self, url):
+		self.url = url
+
+	def sendMessage(self, chat_id, text):
+		urllib2.urlopen(self.url + '/sendMessage?chat_id=' + str(chat_id) + '&text=' + text)
 
 
 def main_manager(ips, ports):
-	global ips_info
+	global IPS_INFO
 
 	while True:
 
@@ -38,45 +49,60 @@ def main_manager(ips, ports):
 
 		# find out what services has each host open
 		for host in pinged_hosts:
-			if host not in ips_info.keys():
-				ips_info[host] = [{}, sV_scanner(host, ports)]
+			if host not in IPS_INFO.keys():
+				IPS_INFO[host] = [{'status':'offline', 'timestamp': getCurrentTimestamp()}, sV_scanner(host, ports)]
 			else:
-				ips_info[host].append(sV_scanner(host, ports))
+				IPS_INFO[host].append(sV_scanner(host, ports))
 
-		getDifferences()
+		# we need to update the hosts in the list that now are offline with no services
+		for host in IPS_INFO:
+			if host not in pinged_hosts:
+				IPS_INFO[host].append({'status':'offline', 'timestamp': getCurrentTimestamp()})
 
-		print ips_info
+		print IPS_INFO
+
+		# comments to send to telegram
+		# comments = getDifferencesInText()
 
 
-def getDifferences():
-	global ips_info
+
+##########################################################################
+###########################     TESTING     ##############################
+##########################################################################
+
+
+
+def getDifferencesInText():
+	global IPS_INFO
+
+	responses = []
 
 	# in this section, we need find new hosts in the net  
-	for host in ips_info:
+	for host in IPS_INFO:
 		# last record
-		actual_ports = ips_info[host][-1]
+		actual_ports = IPS_INFO[host][-1]
 		
 		# last -1
-		last_ports = ips_info[host][-2]
+		last_ports = IPS_INFO[host][-2]
 
-		response = ""
+		comment = ""
 
 		if actual_ports == {}:
 			if last_ports == {}:
-				response = ''
+				comment = ''
 			else:
-				response = host + ": wakes up with no ports open"
+				comment = host + ": wakes up with no ports open"
 		elif actual_ports == last_ports:
 			pass
 		elif last_ports == {}:
-			response = host + ": wakes up with ports " + ", ".join([str(port) + " (" + actual_ports[port]['name'] + ")" for port in actual_ports])
+			comment = host + ": wakes up with ports " + ", ".join([str(port) + " (" + actual_ports[port]['name'] + ")" for port in actual_ports])
 		else:
-			response = host + ": modifies his opened ports: " + close_some_and_open_some_services(actual_ports, last_ports)
+			comment = host + ": modifies his opened ports: " + close_some_and_open_some_services(actual_ports, last_ports)
 
-		print response
+		if comment:
+			responses.append(comment)
 
-
-
+	return responses
 
 
 # here we find the differences between the open ports of the last record, and the record we just made
@@ -93,6 +119,15 @@ def close_some_and_open_some_services(actual, last):
 			closed.append(i)
 
 	return "Opened ports: " + ", ".join(opened) + ". Closed ports: " + ", ".join(closed)
+
+
+
+
+##########################################################################
+###########################     TESTING     ##############################
+##########################################################################
+
+
 
 
 
@@ -127,36 +162,42 @@ def sV_scanner(host, ports):
 
 	# when scanning a android, it returns a null array (dont know why)
 	if nm == []:
-		return {}
+		return {'status':'online', 'timestamp': getCurrentTimestamp()}
 
 	try:
 		# androids are tricky
 		if 'tcp' in nm[host]:
-			services = {}
+			services = {'status':'online', 'timestamp': getCurrentTimestamp()}
 			keys = nm[host]['tcp'].keys()
 			for key in keys:
 				# we only want the port and the service name, so thats what we return 
 				services[key] = {'name': nm[host]['tcp'][key]['name']}
 			return services
 		else:
-			return {}
+			return {'status':'online', 'timestamp': getCurrentTimestamp()}
 
 	except KeyError:
 		print "KeyError"
-		return {}
+		return {'status':'online', 'timestamp': getCurrentTimestamp()}
 
+
+# this method returns the current time difference since the start of the app
+def getCurrentTimestamp():
+	return time.time() - INITIAL_TIME
 
 # loads the global variables to be able to use them in the program
 def load_globals():
-	global ips_info
-	ips_info = {}
+	global IPS_INFO
+	global INITIAL_TIME
+	INITIAL_TIME = time.time()
+	IPS_INFO = {}
 
 
 if __name__ == "__main__":
 
 	load_globals()
 
-	ips = '192.168.100.10/24'
+	ips = '192.168.100.0/24'
 	ports = '1, 5, 7, 18, 20, 21, 22, 23, 25, 29, 37, 42, 43, 49, 53, 69, 70, 79, 80, 103, 108, 109, 110, 115, 118, 119, 137, 139, 143, 150, 156, 161, 179, 190, 194, 197, 389, 396, 443, 444, 445, 458, 546, 547, 563, 569, 1080'
 
 	main_manager(ips, ports)
