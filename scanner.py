b6385ba0583ba0ce96aa4e4909865aa69ac7fc0b
Adrian Anton <adanto64@gmail.com>
2016-04-07 00:24:40 +0200

    Bug found! the reference copy makes this so hard god... ç

--- scanner.py
+++ scanner.py
@@ -3,7 +3,7 @@
 import sys
 import nmap
 
-import urllib2
+import requests
 
 import time
 
@@ -30,11 +30,18 @@ global INITIAL_TIME
 
 
 class bot:
-	def __init__(self, url):
-		self.url = url
+	def __init__(self, token, chat_id):
+		self.token = token
+		self.chat_id = chat_id
 
-	def sendMessage(self, chat_id, text):
-		urllib2.urlopen(self.url + '/sendMessage?chat_id=' + str(chat_id) + '&text=' + text)
+	def sendPostMessage(self, text):
+		r = requests.post('https://api.telegram.org/' + self.token + '/sendMessage', data = {'text': text, 'chat_id': self.chat_id})
+		return r
+
+	def getUpdates(self):
+		r = requests.get('https://api.telegram.org/' + self.token + '/getUpdates')
+		return r.text
+		
 
 
 def main_manager(ips, ports):
@@ -51,6 +58,7 @@ def main_manager(ips, ports):
 		for host in pinged_hosts:
 			if host not in IPS_INFO.keys():
 				IPS_INFO[host] = [{'status':'offline', 'timestamp': getCurrentTimestamp()}, sV_scanner(host, ports)]
+
 			else:
 				IPS_INFO[host].append(sV_scanner(host, ports))
 
@@ -64,18 +72,26 @@ def main_manager(ips, ports):
 		# comments to send to telegram
 		comments = getDifferencesInText()
 
-		print comments
+		print IPS_INFO
+
+		sendMessagesToBot(comments)
 
 		raw_input()
 
 
+def sendMessagesToBot(comments):
 
-##########################################################################
-###########################     TESTING     ##############################
-##########################################################################
+	myBot = bot()
+
+	if len(comments) > 0:
+		myBot.sendPostMessage('##### Updates from ' + str(time.strftime("%H:%M:%S") + ' #####'))
+		for com in comments:
+			myBot.sendPostMessage(com)
+
+		myBot.sendPostMessage('##### End of Updates #####')
 
 
-
+# returns the news about all the hosts in an array
 def getDifferencesInText():
 	global IPS_INFO
 
@@ -88,6 +104,7 @@ def getDifferencesInText():
 		
 		# last -1
 		last_ports = IPS_INFO[host][-2]
+		
 
 		comment = ""
 
@@ -109,25 +126,15 @@ def getDifferencesInText():
 		elif last_ports['status'] == 'offline':
 
 			# we were offline, but now we have started listening
-			# delete those keys to make it easy to get the comment (we dont need them anymore)
-			actual_ports.pop("status", None)
-			actual_ports.pop("timestamp", None)
 
-			comment = host + ": wakes up with ports " + ", ".join([str(port) + " (" + actual_ports[port]['name'] + ")" for port in actual_ports])
+			comment = host + ": wakes up with ports " + ", ".join([str(port) + " (" + actual_ports[port]['name'] + ")" for port in actual_ports if port != 'status' and port != 'timestamp'])
 
 		else:
-
-			# delete those keys to make it easy to get the comment (we dont need them anymore)
-
-			actual_ports.pop("status", None)
-			actual_ports.pop("timestamp", None)
-			last_ports.pop("status", None)
-			last_ports.pop("timestamp", None)
-
 			comment = host + ": modifies his opened ports: " + close_some_and_open_some_services(actual_ports, last_ports)
 
 		if comment:
 			responses.append(comment)
+
 
 	return responses
 
@@ -138,24 +145,16 @@ def close_some_and_open_some_services(actual, last):
 	closed = [] 
 
 	for i in actual:
-		if i not in last:
-			opened.append(i)
+		if i != 'status' and i != 'timestamp':
+			if i not in last:
+				opened.append(i)
 
 	for i in last:
-		if i not in actual:
-			closed.append(i)
+		if i != 'status' and i != 'timestamp':
+			if i not in actual:
+				closed.append(i)
 
 	return "Opened ports: " + ", ".join(opened) + ". Closed ports: " + ", ".join(closed)
-
-
-
-
-##########################################################################
-###########################     TESTING     ##############################
-##########################################################################
-
-
-
 
 
 # This scanner finds up hosts in the ips range and returns the ones online in a string 
@@ -224,7 +223,7 @@ if __name__ == "__main__":
 
 	load_globals()
 
-	ips = '192.168.100.0/24'
+	ips = '192.168.100.11'
 	ports = '1, 5, 7, 18, 20, 21, 22, 23, 25, 29, 37, 42, 43, 49, 53, 69, 70, 79, 80, 103, 108, 109, 110, 115, 118, 119, 137, 139, 143, 150, 156, 161, 179, 190, 194, 197, 389, 396, 443, 444, 445, 458, 546, 547, 563, 569, 1080'
 
 	main_manager(ips, ports)
